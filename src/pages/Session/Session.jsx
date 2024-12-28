fc8e10608e2a482e1a7e1ff61e3fab7d80d9bc18
Fidha Ahamed <fidhaahamedpp@gmail.com>
2024-12-28 11:25:31 +0530

    box added

--- src/pages/Session/Session.jsx
+++ src/pages/Session/Session.jsx
@@ -1,11 +1,15 @@
 import Heading from "../../components/Heading"
 import SessionsCard from "./SessionsCard"
+import ContainerBox from "../../components/ContainerBox"
 
 const Session = () => {
   return (
     <div className="bg-black py-10 overflow-hidden">
         <Heading heading="sessions"/>
-        <SessionsCard/>
+        <ContainerBox>
+          <SessionsCard />
+      </ContainerBox>
+
     </div>
   )
 }
