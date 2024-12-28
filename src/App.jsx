1881bc014fe084773e6f5b911660b1a215b18218
Ajay Susanth <ajaysusanth10@gmail.com>
2024-12-28 22:32:21 +0530

    logo and title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hero from "./pages/Hero/Hero";
 import About from "./pages/About/About.jsx";
 import Session from "./pages/Session/Session";
 import Footer from "./pages/Footer/Footer.jsx";
-import Tickets from "./pages/Tickets/Tickets.jsx";
 
 const App = () => {
   return (
@@ -11,7 +10,6 @@ const App = () => {
       <Hero />
       <About />
       <Session />
-      <Tickets />
       <Footer />
     </div>
   );
