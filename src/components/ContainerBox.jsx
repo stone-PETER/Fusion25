7fb2ff75d88c053b56a6cf745024ca73adae71ef
Fidha Ahamed <fidhaahamedpp@gmail.com>
2024-12-28 13:23:27 +0530

    box responsiveness fixed

--- src/components/ContainerBox.jsx
+++ src/components/ContainerBox.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ContainerBox = ({ children }) => {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="w-full sm:w-[90%] md:w-[80%] lg:w-[70%] xl:w-[60%] h-[500px] sm:h-[500px] lg:h-[600px] bg-gradient-to-br from-white/30 rounded-3xl shadow-lg backdrop-blur-md">
+      <div className="w-[90%] md:w-[70%]  h-[500px] sm:h-[500px] lg:h-[600px] bg-gradient-to-br from-white/30 rounded-3xl shadow-lg backdrop-blur-md">
       {children}
       </div>
     </div>
